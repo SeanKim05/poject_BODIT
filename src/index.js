64bca43b4f63b48c2e2125d4190bd3a6f318b480
SeanKim05 <103613612+SeanKim05@users.noreply.github.com>
2022-10-07 22:52:30 +0900

    Fix ReactDOM.render

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import GlobalStyle from './styles/GlobalStyle';
 import Theme from './styles/Theme';
 import { ThemeProvider } from 'styled-components';
 
 import Routes from './Routes';
 
-ReactDOM.render(
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
+root.render(
   <>
     <GlobalStyle />
     <ThemeProvider theme={Theme}>
       <Routes />
     </ThemeProvider>
   </>,
-
-  document.getElementById('root'),
 );
