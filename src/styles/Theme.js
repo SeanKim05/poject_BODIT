cdaf8e4f1cfa463f7882fdb45f47901ead3b4434
Bong <49029756+2021bong@users.noreply.github.com>
2022-10-08 17:53:28 +0900

    Fix: theme 에러 해결 (#10)

--- src/styles/Theme.js
+++ src/styles/Theme.js
@@ -6,5 +6,3 @@ export const green = '#3cc926';
 export const grey = 'rgba(196,196,196,0.3)';
 export const middleGrey = 'rgba(65,65,65,0.4)';
 export const deepGrey = '#828282';
-
-export default Theme;
